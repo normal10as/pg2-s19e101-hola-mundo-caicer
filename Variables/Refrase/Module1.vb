51578e1baef323fefe47b0f8b565a3c799a77834
Caicer <Caicer@DESKTOP-E3I7EJA>
2019-05-08 18:22:10 -0300

    1.15

---
+++ Variables/Refrase/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim teststring As String
+        teststring = "Shopping List"
+        Console.WriteLine("Reemplazar en '{0}' la o por la i", teststring)
+        Console.WriteLine(Replace(teststring, "o", "i"))
+
+
+
+    End Sub
+
+End Module
