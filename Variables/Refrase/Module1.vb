81f9a9b9f2f6a9b8337905fc6e545b251e4337a8
Caicer <Caicer@DESKTOP-E3I7EJA>
2019-05-13 16:57:21 -0300

    1.15

--- Variables/Refrase/Module1.vb
+++ Variables/Refrase/Module1.vb
@@ -6,7 +6,19 @@
         Console.WriteLine("Reemplazar en '{0}' la o por la i", teststring)
         Console.WriteLine(Replace(teststring, "o", "i"))
 
+        Dim palabra, dos, uno As String
+        palabra = "prueba"
+        dos = "hola"
+        Console.WriteLine("reemplazar en '{0}' la '{1}'", palabra, dos)
+        Console.WriteLine(Replace(palabra, palabra, dos))
 
+        Console.WriteLine("ingrese una palabra: Aunque la cadena sea de oro, sirve para lo mismo.")
+        uno = Console.ReadLine()
+        Console.WriteLine("ingrese otra palabra: ")
+        dos = Console.ReadLine()
+        palabra = "Aunque la cadena sea de oro, sirve para lo mismo."
+        Console.WriteLine("Se reemplazara en '{0}' la palabra '{1}' por la palabra '{2}' ", palabra, uno, dos)
+        Console.WriteLine(Replace(palabra, uno, dos))
 
     End Sub
 
